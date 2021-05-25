c6c960607ee11be4444cf7326fd9df28128570ac
Trantrongtan2000 <32.trantrongtan.92@gmail.com>
2021-05-25 10:28:11 +0700

    commit

--- TrongTan_DA.py
+++ TrongTan_DA.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 import math
  
-foo = Image.open("XLA_Python/test.jpg")
+foo = Image.open("test.jpg")
  
 #print(foo.size)
 a=int(input("Nhập tỉ lệ muốn giảm:\n"))
@@ -13,11 +13,11 @@ x, y = foo.size
 x2, y2 = math.floor(x/a), math.floor(y/a)
  
 foo = foo.resize((x2,y2),Image.ANTIALIAS)
-foo.save("XLA_Python/image_scaled.jpg",optimize=True,quality=95)
+foo.save("image_scaled.jpg",optimize=True,quality=95)
  
  
-im = imageio.imread('XLA_Python/test.jpg')
-im2 = imageio.imread('XLA_Python/image_scaled.jpg')
+im = imageio.imread('test.jpg')
+im2 = imageio.imread('image_scaled.jpg')
 fig, ax = plt.subplots(2, 1, figsize=(15, 10))
  
 ax[0].imshow(im);
