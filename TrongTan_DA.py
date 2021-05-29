f94ee09196adfcc186f7f55c3786c4463d97fd0d
Trantrongtan2000 <32.trantrongtan.92@gmail.com>
2021-05-29 07:46:35 +0700

    'up'

--- TrongTan_DA.py
+++ TrongTan_DA.py
@@ -10,12 +10,11 @@ foo = Image.open("test.jpg")
 a=int(input("Nhập tỉ lệ muốn giảm:\n"))
  
 x, y = foo.size
-x2, y2 = math.floor(x/a), math.floor(y/a)
+x2, y2 = math.floor(x*a), math.floor(y*a)
  
 foo = foo.resize((x2,y2),Image.ANTIALIAS)
 foo.save("image_scaled.jpg",optimize=True,quality=95)
- 
- 
+  
 im = imageio.imread('test.jpg')
 im2 = imageio.imread('image_scaled.jpg')
 fig, ax = plt.subplots(2, 1, figsize=(15, 10))
