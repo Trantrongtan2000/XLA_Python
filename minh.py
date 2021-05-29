8d512d17434c7614dcd2fb7224384acaf26ff617
Trantrongtan2000 <32.trantrongtan.92@gmail.com>
2021-05-29 23:25:58 +0700

    commit on 'main'

--- minh.py
+++ minh.py
@@ -3,5 +3,23 @@ import matplotlib.pyplot as plt
 import nibabel as nib
 
 img = nib.load('ds107_sub001_highres.nii')
-data = img.get_data()
-plt.imshow(data[:, :, 0], cmap='gray')
+data= img.get_fdata()
+while True:
+    nhap=int(input('\nNhập lớp muốn xem: '))
+    if nhap >= 0 and nhap <192:
+        break
+    else:
+        print("\nMời nhập lại giá trị từ 0 tới 191") 
+
+a=data[nhap, :, :]
+b=data[:, nhap, :]
+c=data[:, :, nhap]
+fig, ax = plt.subplots(1, 3, figsize=(15, 10))
+
+ax[0].imshow(a,cmap='gray');
+ax[0].set_title('Mặt nhìn từ bên hông')
+ax[1].imshow(b,cmap='gray');
+ax[1].set_title('Mặt nhìn từ trước tới')
+ax[2].imshow(c,cmap='gray');
+ax[2].set_title('Mặt nhìn từ trên xuống')
+plt.show()
--- test.py
+++ test.py
@@ -1,7 +1,37 @@
-from PIL import Image
+import matplotlib
+import numpy as np
 import matplotlib.pyplot as plt
+import skimage
+import nibabel as nib
+import mcubes
 
-im = Image.open('test.jpg')
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-plt.imshow(im)
+img = nib.load('ds107_sub001_highres.nii')
+
+#data = np.asanyarray(img.dataobj)
+data= img.get_fdata()
+
+#plt.imshow(data[:, :, 30], cmap='gray')
+
+#data_1d = data.ravel()
+#plt.hist(data_1d, bins=100)
+#plt.show()
+
+subvolume = data[90:-90, 90:-90, 10:130]
+pct_99 = np.percentile(subvolume, 99)
+binarized_subvolume = subvolume > pct_99
+
+def binarized_surface(binary_array):
+    verts, faces= mcubes.marching_cubes(binary_array, 0)
+    fig = plt.figure(figsize=(10, 12))
+    ax = fig.add_subplot(111, projection='3d')
+    mesh = Poly3DCollection(verts[faces], linewidths=0, alpha=0.5)
+    ax.add_collection3d(mesh)
+    ax.set_xlim(0, binary_array.shape[0])
+    ax.set_ylim(0, binary_array.shape[1])
+    ax.set_zlim(0, binary_array.shape[2])
+    
+#plt.imshow(binarized_subvolume[:, :, 20], cmap='gray')
+binarized_surface(binarized_subvolume)
 plt.show()
